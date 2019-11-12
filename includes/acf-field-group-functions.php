3ff443c77dad93e1f33304f722382217c330c1e6
James Boynton <james.boynton0@gmail.com>
2019-11-11 21:45:29 -0500

    Update to version 5.8.6

--- includes/acf-field-group-functions.php
+++ includes/acf-field-group-functions.php
@@ -52,7 +52,6 @@ function acf_get_field_group( $id = 0 ) {
 	 *
 	 * @param	array The field_group array.
 	 */
-	$field_group = apply_filters( 'acf/get_field_group', $field_group );
 	$field_group = apply_filters( 'acf/load_field_group', $field_group );
 	
 	// Store field group using aliasses to also find via key, ID and name.
@@ -222,6 +221,10 @@ function acf_validate_field_group( $field_group = array() ) {
 		'description'			=> '',
 	));
 	
+	// Convert types.
+	$field_group['ID'] = (int) $field_group['ID'];
+	$field_group['menu_order'] = (int) $field_group['menu_order'];
+	
 	// Field group is now valid.
 	$field_group['_valid'] = 1;
 	
@@ -319,7 +322,6 @@ function acf_get_field_groups( $filter = array() ) {
 		}
 	}
 	
-	
 	/**
 	 * Filters the $field_groups array.
 	 *
@@ -329,7 +331,6 @@ function acf_get_field_groups( $filter = array() ) {
 	 * @param	array $field_groups The array of field_groups.
 	 */
 	$field_groups = apply_filters( 'acf/load_field_groups', $field_groups );
-	$field_groups = apply_filters( 'acf/get_field_groups', $field_groups );
 	
 	// Filter results.
 	if( $filter ) {
@@ -455,7 +456,7 @@ function acf_get_field_group_visibility( $field_group, $args = array() ) {
 			// Loop over rules and determine if all rules match.
 			$match_group = true;
 			foreach( $group as $rule ) {
-				if( !acf_match_location_rule( $rule, $screen ) ) {
+				if( !acf_match_location_rule( $rule, $screen, $field_group ) ) {
 					$match_group = false;
 					break;
 				}
@@ -518,7 +519,12 @@ function acf_update_field_group( $field_group ) {
     	'post_excerpt'	=> sanitize_title( $field_group['title'] ),
     	'post_content'	=> maybe_serialize( $_field_group ),
     	'menu_order'	=> $field_group['menu_order'],
+    	'comment_status' => 'closed',
+    	'ping_status'	=> 'closed',
 	);
+	
+	// Unhook wp_targeted_link_rel() filter from WP 5.1 corrupting serialized data.
+	remove_filter( 'content_save_pre', 'wp_targeted_link_rel' );
 	
 	// Slash data.
 	// WP expects all data to be slashed and will unslash it (fixes '\' character issues).
@@ -959,7 +965,7 @@ function acf_prepare_field_group_for_export( $field_group = array() ) {
 function acf_import_field_group( $field_group ) {
 	
 	// Disable filters to ensure data is not modified by local, clone, etc.
-	acf_disable_filters();
+	$filters = acf_disable_filters();
 	
 	// Validate field group.
 	$field_group = acf_get_valid_field_group( $field_group );
@@ -1032,7 +1038,12 @@ function acf_import_field_group( $field_group ) {
 			} else {
 				$count[ $field['parent'] ]++;
 			}
-			$field['menu_order'] = ($count[ $field['parent'] ] - 1);
+			
+			// Only add menu order if doesn't already exist.
+			// Allows Flexible Content field to set custom order.
+			if( !isset($field['menu_order']) ) {
+				$field['menu_order'] = ($count[ $field['parent'] ] - 1);
+			}
 			
 			// Save field.
 			$field = acf_update_field( $field );
@@ -1046,7 +1057,7 @@ function acf_import_field_group( $field_group ) {
 	$field_group = acf_update_field_group( $field_group );
 	
 	// Enable filters again.
-	acf_enable_filters();
+	acf_enable_filters( $filters );
 	
 	/**
 	 * Fires immediately after a field_group has been imported.
